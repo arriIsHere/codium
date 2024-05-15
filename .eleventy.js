2faba25efcc921982806aa1b272ab41585ccd481
Arriana Blais <arriblais@gmail.com>
2024-05-15 10:17:21 -0400

    add sass config

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,8 @@
 
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 const pluginWebC = require('@11ty/eleventy-plugin-webc');
+const sass = require('sass');
+const path = require('path');
 
  module.exports = (eleventyConfig) => {
  
@@ -13,9 +15,35 @@ const pluginWebC = require('@11ty/eleventy-plugin-webc');
 	 eleventyConfig.addPlugin(pluginWebC, {
 		components: "_webc/**/*.webc",
 	 });
+
+	 eleventyConfig.addExtension("scss", {
+		outputFileExtension: "css",
+		compile: async function (inputContent, inputPath) {
+		  // Skip files like _fileName.scss
+		  let parsed = path.parse(inputPath);
+		  if (parsed.name.startsWith("_")) {
+			return;
+		  }
+	  
+		  // Run file content through Sass
+		  let result = sass.compileString(inputContent, {
+			loadPaths: [parsed.dir || "."],
+			sourceMap: false, // or true, your choice!
+		  });
+	  
+		  // Allow included files from @use or @import to
+		  // trigger rebuilds when using --incremental
+		  this.addDependencies(inputPath, result.loadedUrls);
+	  
+		  return async () => {
+			return result.css;
+		  };
+		},
+	  });
  
 	 return {
 		 templateFormats: [
+			"scss",
 			"css",
 			"md",
 			"html",
